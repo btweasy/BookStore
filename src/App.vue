fa084c18085159a7da7b956a7b9983cb8b74abe6
Artur Yedomakha <mister.domakha@gmail.com>
2025-02-03 00:26:04 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+git --versionimport RegistrationPage from '@/components/RegistrationPage/RegistrationPage.vue';
+
+export default {
+  components: {
+    Registration: RegistrationPage
+  }
+};
+</script>
+
+<template>
+  <div class="container">
+    <Registration/>
+  </div>
+</template>
+
+<style>
+.container {
+  text-align: center;
+}
+</style>
+
+
+
---
+++ src/components/RegistrationPage/RegistrationPage.vue
@@ -0,0 +1,47 @@
+<style module src="registration.module.css"></style> //запомни
+
+<template>
+  <div :class="$style.container">
+    <div :class="$style.imageBackground"></div>
+
+    <div :class="$style.formSide">
+      <img src="@/components/Icons/logo.png" alt="Book Store Logo" :class="$style.imageLogo">
+      <p :class="$style.welcome">Welcome</p>
+      <h3 :class="$style.h3">Register now</h3>
+
+      <form @submit.prevent="register">
+        <label for="name">Name</label>
+        <input type="text" id="name" v-model="name" placeholder="John Doe" required>
+
+        <label for="email">E-mail</label>
+        <input type="email" id="email" v-model="email" placeholder="john@mail.com" required>
+
+        <label for="password">Password</label>
+        <input type="password" id="password" v-model="password" placeholder="********" required>
+
+        <button type="submit" :class="$style.registerBtn">Register</button>
+      </form>
+      <button :class="$style.loginBtn">Login</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    register() {
+      alert(`User Registered:\nName: ${this.name}\nEmail: ${this.email}`);
+      this.name = "";
+      this.email = "";
+      this.password = "";
+    }
+  }
+};
+</script>
